733624f09e1102daf1d3b573bf9403874829b8ff
Alex Git <avsmaglo@gmail.com>
2019-02-26 12:59:28 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {SectionSalesComponent} from '../app/sections/section-sales/section-sales.component';
+import {SectionOrdersComponent} from '../app/sections/section-orders/section-orders.component';
+import {SectionHealthComponent} from '../app/sections/section-health/section-health.component';
+
+const routes: Routes = [
+  {path: 'sales', component: SectionSalesComponent},
+  {path: 'orders', component: SectionOrdersComponent},
+  {path: 'health', component: SectionHealthComponent}  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
